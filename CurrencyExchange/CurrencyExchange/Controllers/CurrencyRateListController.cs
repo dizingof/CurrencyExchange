63a2cda05bf473902755c7bbf9b737241ea0af91
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-11-24 00:58:48 +0200

    build

--- CurrencyExchange/CurrencyExchange/Controllers/CurrencyRateListController.cs
+++ CurrencyExchange/CurrencyExchange/Controllers/CurrencyRateListController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CurrencyExchange.Contracts.UseCases;
-using Microsoft.AspNetCore.Http;
+﻿using CurrencyExchange.Contracts.UseCases;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
