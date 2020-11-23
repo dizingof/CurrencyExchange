33c659cd5a6726258b7b8334f44fb5972a14a689
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-11-23 20:56:07 +0200

    instal nuget, add structure, add model

---
+++ CurrencyExchange/CurrencyExchange/Models/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Models
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public string Buy { get; set; }
+        public string Sell { get; set; }
+    }
+}
